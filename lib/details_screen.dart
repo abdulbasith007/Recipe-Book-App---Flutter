4d94666ad03a825285f2d5b8361d1e411c5adcb2
abdulbasith007 <abdulbasithCOC@gmail.com>
2024-09-23 17:50:52 -0400

    Added details_screen consisting of the details of each recipe

---
+++ lib/details_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  final Map<String, String> recipe;
+
+  DetailsScreen({required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(recipe['name']!),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Ingredients:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+            SizedBox(height: 8),
+            Text(recipe['ingredients']!),
+            SizedBox(height: 16),
+            Text('Instructions:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+            SizedBox(height: 8),
+            Text(recipe['instructions']!),
+          ],
+        ),
+      ),
+    );
+  }
+}
